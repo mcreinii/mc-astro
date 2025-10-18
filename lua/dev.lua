e96e3ab16873579580c59101d45e09604e0c379f
mcreinii <mcreinii@gmail.com>
2025-10-18 18:49:21 +0000

    Disabled dev mode.
    Not everyone will use it anyway.

--- lua/dev.lua
+++ lua/dev.lua
@@ -1,4 +1,4 @@
--- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is mainly for developing plugins.
