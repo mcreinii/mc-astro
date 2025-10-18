2205999b530c221cdd48c322f8c620e802e1a456
mcreinii <mcreinii@gmail.com>
2025-10-18 17:28:27 +0000

    Added smear effects.
    I added smear effects for my eyes that can't follow quick movements.

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -76,4 +76,14 @@ return {
       )
     end,
   },
+
+  -- Actual added stuff.
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.6,
+      trailing_stiffness = 0.5,
+      distance_stop_animating = 0.5,
+    },
+  },
 }
