47a1c6fb9ac14c9abcd23c221f7ab8a3008d12f2
mcreinii <mcreinii@gmail.com>
2025-10-19 03:01:39 +0000

    Renamed initial todo file.
    Originally named README.md but it didn't make sense for a todo list.
    Rename: TODO.md

--- lua/dev.lua
+++ lua/dev.lua
@@ -5,7 +5,7 @@ if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 vim.notify("Developer lua is enabled.", vim.log.levels.INFO)
 require("mc-todo").setup {
-  initial_target_file = "READMEa.md",
+  initial_target_file = "TODO.md",
   fallback_file = "~/notes/todo.md",
 }
 vim.keymap.set("n", "<leader>mt", "<CMD>MTodo<CR>", { silent = true })
