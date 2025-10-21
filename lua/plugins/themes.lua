3f3f73016b19597e4caa9e81b52766006166e260
mcreinii <mcreinii@gmail.com>
2025-10-21 10:37:54 +0800

    Added a set of themes for users to use.
    Kanagawa
    Catppuccin
    Moonfly
    Tokyonight
    and etc. (I forgot)

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,32 @@
+-- Insert your themes here, I suggest putting more than one for quick switching.
+
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    priority = 1000,
+    lazy = false,
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    priority = 1000,
+    lazy = false,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      theme = "dragon",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    priority = 1000,
+    lazy = false,
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    lazy = false,
+  },
+}
